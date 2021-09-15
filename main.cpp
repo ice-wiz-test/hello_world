66161f380ac27398a237c2620ea08a9f650389d6
KATANADIE <larichev.po@students.sch2.ru>
2021-09-15 10:50:41 +0300

    Changed file

--- main.cpp
+++ main.cpp
@@ -1,8 +1,46 @@
 #include <iostream>
 #include <fstream>
 
+<<<<<<< HEAD
 
+=======
+void printOutAnswers()
+{
+    ifstream fin("answers.txt");
+    string s;
+    for(int i = 0; i < 3; ++i)
+    {
+        fin >> s;
+        cout << s << endl;
+    }
+    fin.close();
+}
+>>>>>>> feature/results
+
+void checkQuestions()
+{
+    <<<<<<< HEAD
+            =======
+                    ifstream fin("questions.txt");
+    ofstream fout("answers.txt");
+    string s;
+    string answer;
+    for(int i = 0; i < 3; ++i)
+    {
+        fin >> s;
+        cout << s << endl;
+        cin >> answer;
+        fout << answer << endl;
+    }
+    fin.close();
+    fout.close();
+}
 
 signed main()
 {
+    string s;
+    cin >> s;
+    if(s == "OPROS") checkQuestions();
+    else printOutAnswers();
+    >>>>>>> feature/results
 }
