4703c357b96ff2cbf0b73cc5113110ba3b836df8
KATANADIE <larichev.po@students.sch2.ru>
2021-09-15 10:48:47 +0300

    Trying to resolve conflicts

--- main.cpp
+++ main.cpp
@@ -1,31 +1,8 @@
 #include <iostream>
 #include <fstream>
 
-void printOutAnswers()
-{
-    ifstream fin("answers.txt");
-    string s;
-    for(int i = 0; i < 3; ++i)
-    {
-        fin >> s;
-        cout << s << endl;
-    }
-    fin.close();
-}
+
 
 signed main()
 {
-    ifstream fin("questions.txt");
-    ofstream fout("answers.txt");
-    string s;
-    string answer;
-    for(int i = 0; i < 3; ++i)
-    {
-        fin >> s;
-        cout << s << endl;
-        cin >> answer;
-        fout << answer << endl;
-    }
-    fin.close();
-    fout.close();
 }
