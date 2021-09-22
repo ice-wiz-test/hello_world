b931f62f973d4e12ca911b8fdedcf1e4997903ec
KATANADIE <larichev.po@students.sch2.ru>
2021-09-22 10:17:02 +0300

    Fixing the code + the function now appends to end of file

--- main.cpp
+++ main.cpp
@@ -1,78 +1,86 @@
 #include <iostream>
 #include <fstream>
-<<<<<<< HEAD
+#include <string>
+#include <map>
+#include <vector>
+#include <algorithm>
 
+using namespace std;
+int total = 0;
+map<string, int> books;
 void putOut(string x)
 {
-    cout << x << endl;
-    =======
-    map<string, int> books;
-    int total = 0;
-    void putOut(string x)
-    {
-        cout << x << endl;
-        int pos = x.rfind(" ");
-        string temp = x.substr(pos + 1, x.size() - pos - 1);
-        total = total + 1 -  min(books[temp], 1);
-        books[temp]++;
+    cout << x << "XX" << endl;
+    string space_delimiter = " ";
+    vector<string> words{};
+
+    size_t pos = 0;
+    while ((pos = x.find(space_delimiter)) != string::npos) {
+        words.push_back(x.substr(0, pos));
+        x.erase(0, pos + space_delimiter.length());
     }
+    string temp = words[words.size() - 1];
+    cout << temp << endl;
+    cout << books[temp] << endl;
+    total = total + 1 - min(books[temp], 1);
+    cout << total << endl;
+    books[temp]++;
+}
 
-    void printOutAnswers(int numberOfField) {
-        total = 0;
-        ifstream fin("answers.txt");
-        string s;
-        vector <string> names;
-        names.clear();
-        if (numberOfField == 1) {
-            while (fin >> s) {
-                {
-                    names.push_back(s);
-                }
-                sort(names.begin(), names.end());
-            }
-        }
-
-        if (numberOfField == 2) {
-            while(fin >> s)
+void printOutAnswers(int numberOfField) {
+    ifstream fin("answers.txt");
+    string s;
+    vector <string> names;
+    names.clear();
+    if (numberOfField == 1) {
+        while (getline(fin, s)) {
             {
-                int pos = s.find(" ");
-                string temp = s.substr(pos, s.rfind(" "));
                 names.push_back(s);
             }
             sort(names.begin(), names.end());
         }
-
-        if (numberOfField == 3) {
-            while(fin >> s)
-            {
-                int pos = s.rfind(" ");
-                string temp = s.substr(pos, s.size() - pos);
-                names.push_back(s);
-            }
-            sort(names.begin(), names.end());
-        }
-
-
     }
-    for(auto x : names)
+
+    if (numberOfField == 2) {
+        while (fin >> s)
+        {
+            int pos = s.find(" ");
+            string temp = s.substr(pos, s.rfind(" "));
+            names.push_back(s);
+        }
+        sort(names.begin(), names.end());
+    }
+
+    if (numberOfField == 3) {
+        while (fin >> s)
+        {
+            int pos = s.rfind(" ");
+            string temp = s.substr(pos, s.size() - pos);
+            names.push_back(s);
+        }
+        sort(names.begin(), names.end());
+    }
+
+    books.clear();
+    for (auto x : names)
     {
         putOut(x);
     }
-
+    cout << total << endl;
     cout << "Amount of people who passed the survey - " << names.size() << endl;
     cout << "Amount of books present - " << total;
+    total = 0;
     fin.close();
 }
 
 void checkQuestions()
 {
     ifstream fin("questions.txt");
-    ofstream fout("answers.txt");
+    ofstream fout("answers.txt", std::ios_base::app);
     string s;
     string answer;
-    for(int i = 0; i < 3; ++i)
+    while (getline(fin, s))
     {
-        fin >> s;
         cout << s << endl;
         cin >> answer;
         fout << answer << " ";
@@ -85,7 +93,9 @@ void checkQuestions()
 signed main()
 {
     string s;
+    cout << "If you want to complete the survey, print out 'SURVEY' " << endl;
+    cout << "If you want to know the results of the survey, print out 'RESULTS'" << endl;
     cin >> s;
-    if(s == "OPROS") checkQuestions();
-    else printOutAnswers();
+    if (s == "SURVEY") checkQuestions();
+    if (s == "RESULTS") printOutAnswers(1);
 }
